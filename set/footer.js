eb58e7755822a6fd2ffdf75950ffe19a37580804
Cco0orn <heyislel673@gmail.com>
2025-04-27 20:16:04 +0800

    Add files via upload

---
+++ set/footer.js
@@ -0,0 +1,3 @@
+document.getElementById('footer').innerHTML = `
+     <p>Since 2024. All rights to the <i>Kuru Kuru Kururin</i> series belong to Eighting and Nintendo, this website is fan-made and not affiliated with Eighting and Nintendo.</p>
+`;
